ff43f0dbceaf321042fa55a23e90b96bc7d5dcfe
jueun <sksmsdiwndmsdl2001@gmail.com>
2022-07-26 03:26:28 +0900

    toy project

---
+++ RISC-V/c_project/grade_program.c
@@ -0,0 +1,31 @@
+// grade program
+
+int main() {
+    int num1 = 100, num2 = 67;
+    int total = 0;
+    int avg = 0;
+    
+    int num;
+    int grade;
+
+    total = num1 + num2;
+    avg = total / 2;
+    
+    switch(num) {
+        L0 :            
+            grade = 1;
+            break;
+        L1 :
+            grade = 2;
+            break;
+        L2 :
+            grade = 3;
+            break;
+        L3 :
+            grade = 4;
+            break;
+        default :
+            grade = 9;
+            break;
+    }
+}
