bf90ef094981cd3d33970c95eafac68b91ccc9b4
juxge <sksmsdiwndmsdl2001@gmail.com>
2022-07-25 15:31:00 +0900

    toy project

--- RISC-V/asm/ex01_power_of_two.s
+++ RISC-V/asm/ex01_power_of_two.s
@@ -18,17 +18,16 @@ _start:
 
 # t0 : n, t1: a
 init:
-	li	t0, 1	## n = 1
-	li	t1, 1	## a = 1
-	li	t4, 1	## shift left 2
+	li	t0, 1			## n = 1
+	li	t1, 1			## a = 1
+	li	t4, 1			## shift left 2
 	
 Square:
 	addi	t2, t0, -10		## t2 = n - 10
-	bge	t2, zero, L0		## if (t2 >= 0) go to L0
-	
-L0:
+	bge	t2, zero, done		## if (t2 >= 0) go to done
 	sll	t1, t1, t4		## t1 = t1 << 1 
-						## a = 2*a
-	addi	t0, t0, 1	## n = n + 1
+					## a = 2*a
+	addi	t0, t0, 1		## n = n + 1
 	j Square
 	
+done: mv t5, t1				## last a is storeed in t5
---
+++ power_of_two.c
@@ -0,0 +1,11 @@
+// power of two(hex)
+
+int main()
+{
+	int n=1, a=1;
+	while(n<10)
+	{
+		a = 2*a;
+		n+=1;
+    }
+}
